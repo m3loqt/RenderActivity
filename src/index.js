bd7c7058d254bb5ceca4fe3c4242177f17269e38
Melo <mees.cortes.swu@phinmaed.com>
2024-08-30 15:16:43 +0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express"
+import ProfileRoute from './routes/profile.js';
+
+
 
 const app = express();
 app.use(express.json());
@@ -9,14 +12,14 @@ app.get('/', (req, res) => {
     res.status(200).send('Welcome to my server');
 });
 
+
+app.use(ProfileRoute)
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
     console.log(`Server Started on port ${PORT}`);
 });
 
-app.get('/myprofile', (req, res) => {
-    res.status(200).send({name: 'Mel', address: "Cebu City", age: 23});
-});
+
 
 
